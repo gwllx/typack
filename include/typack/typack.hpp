a4bfe2cf19f8e4f88770cdeba8ef7c032a4e13ae
Gareth Williamson <gwllx.dev@gmail.com>
2023-10-16 17:00:05 +0200

    Add pack concat and remove APIs

--- include/typack/typack.hpp
+++ include/typack/typack.hpp
@@ -5,29 +5,99 @@
 
 namespace ty {
 
-template<typename ...Ts>
-struct pack {
-    static constexpr auto size = sizeof...(Ts);
+template<typename ...> struct pack;
+
+///
+/// Empty specialisation
+///
+template<>
+struct pack<> {
+    static constexpr auto size = std::size_t{0};
+
+    ///
+    /// Concat
+    ///
+    template<typename ...Ts>
+    struct concat { using type = pack<Ts...>; };
+
+    template<typename ...Ts>
+    struct concat<pack<Ts...>> { using type = pack<Ts...>; };
+
+    template<typename ...Ts>
+    using concat_t = typename concat<Ts...>::type;
+
+    ///
+    /// Remove
+    ///
+    template<typename ...Ts>
+    struct remove { using type = pack<>; };
+
+    template<typename ...Ts>
+    using remove_t = typename remove<Ts...>::type;
 };
 
-using empty = pack<>;
+using empty_t = pack<>;
 
+///
+/// Main specialisation
+///
 template<typename Head, typename ...Tail>
 struct pack<Head, Tail...> {
-    using head = Head;
-    using tail = pack<Tail...>;
-    static constexpr std::size_t size = sizeof...(Tail) + 1;
+    static constexpr auto size = sizeof...(Tail) + std::size_t{1};
 
+    using head_t = Head;
+    using tail_t = pack<Tail...>;
+
+    ///
+    /// Random access
+    ///
     template<std::size_t N>
-    struct nth {
-        using type = typename tail::template nth<N - 1>::type;
+    struct at {
+        using type = typename tail_t::template at<N - 1>::type;
+    };
+
+    template<> struct at<0> { using type = head_t; };
+    template<std::size_t N> using at_t = typename at<N>::type;
+
+    ///
+    /// Concat
+    ///
+    template<typename ...Ts>
+    struct concat {
+        using type = pack<Head, Tail..., Ts...>;
+    };
+
+    template<typename ...Ts>
+    struct concat<pack<Ts...>> { using type = typename concat<Ts...>::type; };
+
+    template<typename ...Ts>
+    using concat_t = typename concat<Ts...>::type;
+
+    ///
+    /// Remove
+    ///
+    template<typename...> struct remove;
+
+    template<typename First, typename ...Ts>
+    struct remove<First, Ts...> {
+        using rm_tail_t = typename tail_t::template remove<First>::type;
+        using rm_first_t = typename pack<Head>::template concat_t<rm_tail_t>;
+        using type = typename rm_first_t::template remove<Ts...>::type;
+    };
+
+    template<typename ...Ts>
+    struct remove<Head, Ts...> {
+        using type = typename tail_t::template remove<Head, Ts...>::type;
     };
 
     template<>
-    struct nth<0> { using type = head; };
+    struct remove<> { using type = pack<Head, Tail...>; };
 
-    template<std::size_t N>
-    using at = typename nth<N>::type;
+    template<typename ...Ts>
+    struct remove<pack<Ts...>> { using type = typename remove<Ts...>::type; };
+
+    template<typename ...Ts>
+    using remove_t = typename remove<Ts...>::type;
 };
 
 } // namespace ty
--- test/typack_test.cpp
+++ test/typack_test.cpp
@@ -1,17 +1,51 @@
 #include "typack/typack.hpp"
 #include <type_traits>
 
-int main() {
-    using uut = ty::pack<int, float, char>;
+#define IS_SAME(...) static_assert(std::is_same_v<__VA_ARGS__>)
 
-    static_assert(ty::empty::size == 0);
-    static_assert(std::is_same_v<ty::pack<>, ty::empty>);
-    static_assert(uut::size == 3);
-    static_assert(std::is_same_v<uut::head, int>);
-    static_assert(std::is_same_v<uut::tail, ty::pack<float, char>>);
-    static_assert(std::is_same_v<uut::at<0>, int>);
-    static_assert(std::is_same_v<uut::at<1>, float>);
-    static_assert(std::is_same_v<uut::at<2>, char>);
+int main() {
+    using namespace ty;
+    using uut = pack<int, float, char>; // Unit under test
+
+    static_assert(uut::size == std::size_t{3});
+    IS_SAME(uut::head_t, int);
+    IS_SAME(uut::tail_t, pack<float, char>);
+
+    static_assert(empty_t::size == 0);
+    IS_SAME(pack<>, empty_t);
+
+    // Random access
+    IS_SAME(uut::at_t<0>, int);
+    IS_SAME(uut::at_t<1>, float);
+    IS_SAME(uut::at_t<2>, char);
+
+    // Concat
+    IS_SAME(empty_t::concat_t<int>, pack<int>);
+    IS_SAME(empty_t::concat_t<float, double>, pack<float, double>);
+    IS_SAME(empty_t::concat_t<pack<char, int>>, pack<char, int>);
+
+    IS_SAME(uut::concat_t<double>, pack<int, float, char, double>);
+    IS_SAME(uut::concat_t<float, int>, pack<int, float, char, float, int>);
+
+    using uut2 = pack<int, double>;
+    IS_SAME(uut::concat_t<uut2>, pack<int, float, char, int, double>);
+
+    // Remove
+    IS_SAME(empty_t::remove_t<>, empty_t);
+    IS_SAME(empty_t::remove_t<double>, empty_t);
+    IS_SAME(empty_t::remove_t<pack<int, char>>, empty_t);
+
+    IS_SAME(uut::remove_t<float>, pack<int, char>);
+    IS_SAME(uut::remove_t<int, char>, pack<float>);
+    IS_SAME(uut::remove_t<pack<char, float>>, pack<int>);
+
+    // Something silly...
+    IS_SAME(char,
+            pack<int, char>
+                ::concat_t<pack<float, double>>
+                ::remove_t<float>
+                ::tail_t
+                ::head_t);
 
     return 0;
 }
